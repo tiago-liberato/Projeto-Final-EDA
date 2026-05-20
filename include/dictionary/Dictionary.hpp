3bc4395fc3b7e8a953d24ef5d397199c69c60e2f
well204 <well.santos@alu.ufc.br>
2026-05-20 18:02:21 -0300

    alterando os tipos da classe dictionary

--- include/dictionary/Dictionary.hpp
+++ include/dictionary/Dictionary.hpp
@@ -15,11 +15,11 @@ class Dictionary{
   
     virtual void insert(K& key, V& value) = 0;
     virtual void update(K& key, V& value) = 0;
-    virtual V& acess(K& key) = 0;
+    virtual V get(const K& key) const = 0;
     virtual void remove(K& key) = 0;
-    virtual bool contains(K& key) = 0;
-    virtual void size() = 0;
-    virtual clear() = 0;
+    virtual bool contains(K& key) const = 0;
+    virtual std::size_t size() const = 0;
+    virtual void clear() = 0;
 };
 
 #endif
